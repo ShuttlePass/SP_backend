01aafcd785e6ce582cede015c7f20f074fe8a871
bon9 <rnem8831@gmail.com>
2025-01-16 14:32:17 +0900

    scripts-build

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // ValidationPipe 설정
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, // DTO 객체로 변환
+    whitelist: true, // 유효하지 않은 속성 필터링
+    forbidNonWhitelisted: true, // 화이트리스트에 없는 속성이 있으면 예외 발생
+  }));
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
